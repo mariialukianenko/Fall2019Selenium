1f73b87ac6f079e96b2dec3a63be2925bfea430c
mariialukianenko <lukianenko.mariia@gmail.com>
2020-03-28 12:20:59 -0500

    commit

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package com.automation.pages;
+
+import com.automation.utilities.ConfigurationReader;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+    // public WebElement username2 = Driver.getDriver().findElement(By.id("prependedInput"));
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(id = "_submit")
+    public WebElement login;
+
+    @FindBy(id = "Forgot your password")
+    public WebElement forgotPassword;
+
+
+    /**
+     * Method to login, version #1
+     * Login as a specific user
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public LoginPage(){
+        //to connect our webdriver, page class and page factory
+        //PageFactory - used to use @FindBy annotations
+        //PageFactory - helps to find elements easier
+        PageFactory.initElements(Driver.getDriver(), this); //instead "this", we can say "LoginPage.class"
+    }
+
+    public void login (String usernameValue, String passwordValue){
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue);
+    }
+
+    /**
+     * Method to login, version #2
+     * Login as a default user
+     * Credentials will be retrieved from configuration.properties file
+     */
+    public void login(){
+        username.sendKeys(ConfigurationReader.getProperty("store_manager"));
+        password.sendKeys(ConfigurationReader.getProperty("password"), Keys.ENTER);
+    }
+
+}
