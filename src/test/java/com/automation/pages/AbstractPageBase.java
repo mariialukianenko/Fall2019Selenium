5a0dee83e968f4afd1adb0ad46e59b8d3a096b39
mariialukianenko <lukianenko.mariia@gmail.com>
2020-03-28 16:04:10 -0500

    commit

--- src/test/java/com/automation/pages/AbstractPageBase.java
+++ src/test/java/com/automation/pages/AbstractPageBase.java
@@ -1,7 +1,11 @@
 package com.automation.pages;
 
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 /**
@@ -20,9 +24,28 @@ public class AbstractPageBase {
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Method for vytrack navigation. Provide tab name and module name to navigate
+     * @param tabName, like Dashboards, Fleet or Customers
+     * @param moduleName, like Vehicles, Vehicles Odometer and Vehicles Costs
+     */
     public void navigateTo(String tabName, String moduleName){
-        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'"+tabName+"')]";
+        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'" + tabName + "')]";
+        String moduleXpath = "//span[@class='title title-level-2' and text()='" + moduleName + "']";
 
+        WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
+        WebElement moduleElement = driver.findElement(By.xpath(moduleXpath));
+
+        Actions actions = new Actions(driver);
+
+        BrowserUtils.wait(4);
+
+        actions.moveToElement(tabElement).
+                pause(2000).
+                click(moduleElement).
+                build().perform();
+
+        BrowserUtils.wait(4);
     }
 
 }
