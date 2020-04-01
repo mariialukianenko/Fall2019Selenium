ff71b10d8dd73b24051c912fcb6ba6fc13f511ee
mariialukianenko <lukianenko.mariia@gmail.com>
2020-03-31 20:56:59 -0500

    commit

--- src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
@@ -1,18 +1,50 @@
 package com.automation.tests.vytrack;
 
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public abstract class AbstractTestBase {
 
     //will be visible in the subclass, regardless on subclass location (same package or no)
     protected WebDriverWait wait;
     protected Actions actions;
+
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest test;
+
+    @BeforeTest
+    public void setupTest() {
+        report = new ExtentReports();
+        String reportPath = "";
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/report.html";
+        }
+        htmlReporter = new ExtentHtmlReporter(reportPath);
+        //add it to the reportitself
+        report.attachReporter(htmlReporter);
+        htmlReporter.config().setReportName("VYTRACK Test Automation Results");
+
+    }
+
+    @AfterTest
+    public void afterTest(){
+        report.flush(); //to release a report
+    }
 
     @BeforeMethod
     public void setup(){
@@ -25,8 +57,19 @@ public abstract class AbstractTestBase {
 
 
     @AfterMethod
-    public void teardown(){
+    public void teardown(ITestResult iTestResult) throws Exception{
+        //ITestResult class describe the result of a test
+        //if test failed, take a screenshot
+        if (iTestResult.getStatus() == ITestResult.FAILURE){
+            //screnshot will have a name of the test
+           String screenshotPath =  BrowserUtils.getScreenshot(iTestResult.getName());
+            test.fail(iTestResult.getName()); //attach test name that failed
+            BrowserUtils.wait(2);
+            test.addScreenCaptureFromPath(screenshotPath, "Failed"); //attach screenshot
+            test.fail(iTestResult.getThrowable()); //attach console output
+        }
 
+        BrowserUtils.wait(2);
         Driver.closeDriver();
     }
 
--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -2,7 +2,9 @@ package com.automation.tests.vytrack.login;
 
 import com.automation.pages.LoginPage;
 import com.automation.tests.vytrack.AbstractTestBase;
+import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
+import org.apache.commons.compress.compressors.brotli.BrotliUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,9 +15,16 @@ public class NewLoginTests extends AbstractTestBase {
      */
     @Test
     public void verifyPageTitle(){
+        //test --> ExtentTest object
+        //we must add to every test at the beginning
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
+        test.info("Login as store manager"); //log some steps
+        //like system.out, but goes to report as well
         Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        //if assertion passed, it will set test status in report to passed
+        test.pass("Page title Dashboard was verified");
     }
 
     /**
@@ -25,9 +34,15 @@ public class NewLoginTests extends AbstractTestBase {
 
     @Test
     public void verifyWarningMessage(){
+        test = report.createTest("Verify warning massege");
+
         LoginPage loginPage = new LoginPage();
         loginPage.login("wrong", "wrong");
         Assert.assertEquals(loginPage.getWarningMessageText(), "Invalid user name or password.");
+        //take a screenshot
+        BrowserUtils.getScreenshot("warning_message");
+
+        test.pass("Warning messeage is displayed");
     }
 
 }
