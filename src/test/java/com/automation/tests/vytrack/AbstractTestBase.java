f0fb1e452936ebecc5adac9df5d5bd9f09a4a70d
mariialukianenko <lukianenko.mariia@gmail.com>
2020-04-14 20:49:25 -0500

    commit

--- src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
@@ -4,6 +4,7 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.ConfigurationReader;
 import com.automation.utilities.Driver;
 
+import com.automation.utilities.ExcelUtil;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
@@ -25,13 +26,16 @@ public abstract class AbstractTestBase {
     protected ExtentHtmlReporter htmlReporter;
     protected ExtentTest test;
 
+    protected static int row = 1;
+    protected ExcelUtil excelUtil;
+
     //@Optional - to make parameter optional
     //if you don't specify it, testng will require to specify this parameter for every test, in xml runner
     @BeforeTest
     @Parameters("reportName")
     public void setupTest(@Optional String reportName) {
         System.out.println("Report name: " + reportName);
-        reportName = reportName == null ? "report.html" : reportName+".html";
+        reportName = reportName == null ? "report.html" : reportName + ".html";
 
         report = new ExtentReports();
 
@@ -76,6 +80,11 @@ public abstract class AbstractTestBase {
             BrowserUtils.wait(2);
             test.addScreenCaptureFromPath(screenshotPath, "Failed");//attach screenshot
             test.fail(iTestResult.getThrowable());//attach console output
+            //if excelUtil object was created
+            //set value if result column to failed
+            if (excelUtil != null) {
+                excelUtil.setCellData("FAILED", "result", row++);
+            }
         }
         BrowserUtils.wait(2);
         Driver.closeDriver();
